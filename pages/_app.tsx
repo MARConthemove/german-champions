21cc43cb4873ef886007d55f08a008a3686c1755
MARConthemove <marc_onthemove@hotmail.com>
2023-05-10 17:08:31 +0200

    Created companies mock data
    Added new feature #6

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+// import companies from './companies.json'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
